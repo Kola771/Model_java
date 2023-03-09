1eb8c451f050d337c37aa5b6aa0fc58a9f1a237e
Kola771 <koladeaboudou@gmail.com>
2023-03-09 16:48:04 +0100

    update

---
+++ src/App.java
@@ -0,0 +1,9 @@
+import bj.highfive.Exo;
+
+public class App {
+    public static void main(String[] args)
+    {
+        Exo obj = new Exo();
+        obj.Test();
+    }
+}
---
+++ src/bj/highfive/Exo.java
@@ -0,0 +1,10 @@
+package bj.highfive;
+
+public class Exo {
+    public static void Test()
+    {
+        System.out.println("I'm Atomic");
+    }
+
+
+}
